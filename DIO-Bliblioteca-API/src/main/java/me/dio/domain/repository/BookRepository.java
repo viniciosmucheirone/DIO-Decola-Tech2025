a9f0762be6c99656b676c2d9d880dad529a2f329
viniciosmucheirone <viniciosmucheirone@icloud.com>
2025-03-10 18:04:00 -0300

    Atualizando estrutura do projeto

--- DIO-Bliblioteca-API/src/main/java/me/dio/domain/repository/BookRepository.java
+++
@@ -1,8 +0,0 @@
-package me.dio.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import me.dio.domain.model.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long > {
-}
